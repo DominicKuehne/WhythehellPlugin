ec26a8023f247d1d781dbd6cde25c235ecb40ac6
Dominic Kühne <dominic.kuehne@googlemail.com>
2015-05-26 11:19:13 +0200

    kill command added

--- src/WhythehellPlugin.java
+++ src/WhythehellPlugin.java
@@ -4,24 +4,52 @@
 
 import java.util.logging.Logger;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WhythehellPlugin extends JavaPlugin {
-    public static Logger log =Logger.getLogger("Minecraft");
+    public static Logger log = Logger.getLogger("Minecraft");
 
     public void onLoad() {
 
         log.info("[WhythehellPlugin] Loaded...");
 
     }
+
     public void onEnable() {
 
         log.info("[WhythehellPlugin] Starting up...");
 
     }
+
     public void onDisable() {
 
         log.info("[WhythehellPlugin] Shuting down...");
 
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("Whythehell")) {
+
+            if (sender instanceof Player) {
+                String message = "[Server]: YO was geht'n hier?";
+                getServer().broadcastMessage(message);
+                return true;
+            } else {
+                sender.sendMessage("[Server]:Du Kacklappen das kannste so nisch machen!");
+            }
+
+        } else if (command.getName().equalsIgnoreCase("killplayer")) {
+            Player opfer = sender.getServer().getPlayer(args[0]);
+            if (opfer == null) {
+                sender.sendMessage("Player" +args[0]+"is not online!");
+                return true;
+            }
+            opfer.setHealth(0.0);
+        }
+        return false;
     }
 }
